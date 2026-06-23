6ec816cfc500ec305278bad8cf2eb66fdbad1a30
Ilfat Hismatullin <ilfathismatullin39@gmail.com>
2026-06-23 23:22:59 +0300

    Delete start_cp.sh

--- start_cp.sh
+++
@@ -1,5 +0,0 @@
-sudo apt install screen
-sudo apt install tmux
-echo "=== 2. Копирование simulator.launch ==="
-cp -r $PROJECT_DIR/home/clover/Desktop/solar_panel/ /home/clover/catkin_ws/src/sitl_gazebo/models/
-cp $PROJECT_DIR/simulator.launch /home/clover/catkin_ws/src/clover/clover_simulation/launch/simulator.launch
